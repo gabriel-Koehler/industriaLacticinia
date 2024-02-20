942a5e106042b852b89f8208001853f95ddd36bb
Gabriel Koehler da Silva <gabrielks@weg.net>
2024-02-20 16:46:40 -0300

    fundo component

--- src/app/fundo/fundo.component.ts
+++ src/app/fundo/fundo.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-fundo',
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fundo.component.scss']
 })
 export class FundoComponent implements OnInit {
-
+  @Input() texto:String='titulo'
+  @Input() cor:String='#0A93BE'
   constructor() { }
 
   ngOnInit(): void {
